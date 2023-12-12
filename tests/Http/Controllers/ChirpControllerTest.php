006729fcca1dda237562b813cbae7acd46fd2277
arnaud adrion <adrionarnaud.1@gmail.com>
2023-12-12 22:45:41 +0100

    MAJ

---
+++ tests/Http/Controllers/ChirpControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Http\Controllers;
+
+use App\Http\Controllers\ChirpController;
+use PHPUnit\Framework\TestCase;
+
+class ChirpControllerTest extends TestCase
+{
+    public function testIndexAction()
+    {
+
+    }
+}
